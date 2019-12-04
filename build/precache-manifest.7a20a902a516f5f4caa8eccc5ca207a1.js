71b8d1f452ae1fee9950d52563011eb8c13c17df
Luis Villalobos <vlobos323@gmail.com>
2019-12-03 17:20:03 -0800

    moved files

---
+++ build/precache-manifest.7a20a902a516f5f4caa8eccc5ca207a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6872ff7886062e921699502d7516e359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f222e6d18a7e046b27f",
+    "url": "/static/css/main.73401aae.chunk.css"
+  },
+  {
+    "revision": "18ec467e8b956ac6351e",
+    "url": "/static/js/2.92579b31.chunk.js"
+  },
+  {
+    "revision": "9f222e6d18a7e046b27f",
+    "url": "/static/js/main.2bc02fff.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "2f2fe0fe1801f26f799385567a5f89f4",
+    "url": "/static/media/bg.2f2fe0fe.jpg"
+  }
+]);
